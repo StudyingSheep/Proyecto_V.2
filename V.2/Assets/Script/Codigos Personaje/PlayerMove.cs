7a2e9d741d49b23dbcb22afc20ec915a271b10c8
LikeSuperset24 <113186153+LikeSuperset24@users.noreply.github.com>
2022-11-04 23:52:51 -0600

    Animación de golpear

--- V.2/Assets/Script/Codigos Personaje/PlayerMove.cs
+++ V.2/Assets/Script/Codigos Personaje/PlayerMove.cs
@@ -19,6 +19,11 @@ public class PlayerMove : MonoBehaviour
     public float velocidadInicial;
     public float velocidadAgachado;
 
+    // Variables para hacer que el personaje golpeé 
+    public bool estoyAtacando;
+    public bool avanzoSolo;
+    public float impulsoGolpe = 10; 
+
     void Start() {
         puedoSaltar = false; 
         anim = GetComponent<Animator>();
@@ -29,31 +34,43 @@ public class PlayerMove : MonoBehaviour
 
     // Para generalizar los cuadros por segundo en cualquier computadora. 
     void FixedUpdate() {
-        transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
-        transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
+        if (!estoyAtacando) {
+            transform.Rotate(0, x * Time.deltaTime * velocidadRotacion, 0);
+            transform.Translate(0, 0, y * Time.deltaTime * velocidadMovimiento);
+        }
+
+        if (avanzoSolo) {
+            rb.velocity = transform.forward * impulsoGolpe; 
+        }
     }
     void Update() {
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Mouse0) && puedoSaltar && !estoyAtacando) {
+            anim.SetTrigger("Golpeo");
+            estoyAtacando = true; 
+        }
 
         anim.SetFloat("VelX", x);
         anim.SetFloat("VelY", y);
 
         if (puedoSaltar) {
             // Funciona con una ves que se presione la barra espaciadora 
-            if (Input.GetKeyDown(KeyCode.Space)) {
-                anim.SetBool("Salto", true);
-                rb.AddForce(new Vector3(0, fuerzaDeSalto, 0), ForceMode.Impulse); 
+            if (!estoyAtacando) {
+                if (Input.GetKeyDown(KeyCode.Space)) {
+                    anim.SetBool("Salto", true);
+                    rb.AddForce(new Vector3(0, fuerzaDeSalto, 0), ForceMode.Impulse);
+                }
+                // Solo funciona mientras se presiona la tecla 
+                if (Input.GetKey(KeyCode.LeftControl)) {
+                    anim.SetBool("Agacharse", true);
+                    velocidadMovimiento = velocidadAgachado;
+                } else {
+                    anim.SetBool("Agacharse", false);
+                    velocidadMovimiento = velocidadInicial;
+                }
             }
-            // Solo funciona mientras se presiona la tecla 
-            if (Input.GetKey(KeyCode.LeftControl)) {
-                anim.SetBool("Agacharse", true);
-                velocidadMovimiento = velocidadAgachado; 
-            } else {
-                anim.SetBool("Agacharse", false);
-                velocidadMovimiento = velocidadInicial;
-            }
-
             anim.SetBool("TocarSuelo", true);
         } else {
             EstoyCayendo(); 
@@ -64,5 +81,18 @@ public class PlayerMove : MonoBehaviour
     public void EstoyCayendo() {
         anim.SetBool("TocarSuelo", false);
         anim.SetBool("Salto", false);
+    }
+
+    // Metodos para cuando el personaje golpea
+    public void DejarDeGolpear() {
+        estoyAtacando = false;
+    }
+
+    public void AvanzoSolo() {
+        avanzoSolo = true; 
+    }
+
+    public void DejarDeAvanzar() {
+        avanzoSolo = false; 
     }
 }
