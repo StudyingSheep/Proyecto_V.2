c8e41d9116e8bdff678dabee8cf2d5c2052cec16
LikeSuperset24 <113186153+LikeSuperset24@users.noreply.github.com>
2022-11-09 18:48:38 -0600

    Cámara Orbital

---
+++ V.2/Assets/Script/Codigos Personaje/CamaraOrbital.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamaraOrbital : MonoBehaviour
+{
+    private Vector2 angulo = new Vector2(90 * Mathf.Deg2Rad, 0);
+
+    public float distancia;
+    public Transform seguir;
+    public Vector2 sensibilidad; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseHorizontal = Input.GetAxis("Mouse X");
+        if (mouseHorizontal != 0)
+        {
+            angulo.x += mouseHorizontal * Mathf.Deg2Rad * sensibilidad.x; 
+        }
+
+        float mouseVertical = Input.GetAxis("Mouse Y");
+        if (mouseVertical != 0)
+        {
+            angulo.y += mouseVertical * Mathf.Deg2Rad * sensibilidad.y;
+            angulo.y = Mathf.Clamp(angulo.y, -80 * Mathf.Deg2Rad, 80 * Mathf.Deg2Rad); 
+        }
+    }
+
+    public void LateUpdate()
+    {
+        Vector3 orbita = new Vector3(
+            Mathf.Cos(angulo.x) * Mathf.Cos(angulo.y),
+            -Mathf.Sin(angulo.y), 
+            -Mathf.Sin(angulo.x) * Mathf.Cos(angulo.y)
+            );
+        transform.position = seguir.position + orbita * distancia;
+        transform.rotation = Quaternion.LookRotation(seguir.position - transform.position);
+    }
+}
